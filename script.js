4842b8313f10fc7442a8af708b80356e90eed71e
Volodya <example@gmail.com>
2023-02-24 23:17:06 +0200

    19Lesson film DB

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert('script')
+let numberOfFilms = +prompt("how many movie did you watch?");
+let lastWatchedFilm;
+let markFilm;
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+for (let i = 0; i < 2; i++) {
+    lastWatchedFilm = prompt("what is your last watched film?");
+    markFilm = +prompt("give a mark for him");
+    personalMovieDB.movies[lastWatchedFilm] = markFilm;
+};
+
+console.log(numberOfFilms);
+console.log(personalMovieDB);
